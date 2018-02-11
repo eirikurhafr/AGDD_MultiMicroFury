a3cfd41cb3cf971758cc21acb3836180f6661671
Eiríkur H. Friðriksson <eirikurhf@gmail.com>
2018-02-11 17:10:53 +0000

    ballGame WIP

---
+++ AGDD_MultiMicroFury/Assets/Scripts/BallController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public bool isPlayer1;
+    private Rigidbody rb;
+    private Vector3 forwardV;
+    private Vector3 backwardsV;
+    private Vector3 leftV;
+    private Vector3 rightV;
+    public float speed;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+
+        if (isPlayer1)
+        {
+            forwardV = new Vector3(speed, 0, 0);
+            backwardsV = new Vector3(-speed, 0, 0);   
+        }else{
+            forwardV = new Vector3(-speed, 0, 0);
+            backwardsV = new Vector3(speed, 0, 0);
+        }
+
+
+        leftV = new Vector3(0, 0, speed);
+        rightV = new Vector3(0, 0, -speed);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isPlayer1){
+            moveCharacter1(forwardV, backwardsV, leftV, rightV);
+        } else{
+            moveCharacter2(forwardV, backwardsV, leftV, rightV);
+        }
+        hasLost();
+    }
+
+    private void moveCharacter1(Vector3 forward, Vector3 backwards, Vector3 left, Vector3 right)
+    {
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(left);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(forward);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(right);
+        }
+        if (Input.GetKey("a") && Input.GetKey("d"))
+        {
+            rb.AddForce(backwards);
+        }
+
+    }
+
+    private void moveCharacter2(Vector3 forward, Vector3 backwards, Vector3 left, Vector3 right)
+    {
+        if (Input.GetKey("j"))
+        {
+            rb.AddForce(left);
+        }
+        if (Input.GetKey("k"))
+        {
+            rb.AddForce(forward);
+        }
+        if (Input.GetKey("l"))
+        {
+            rb.AddForce(right);
+        }
+        if (Input.GetKey("j") && Input.GetKey("l"))
+        {
+            rb.AddForce(backwards);
+        }
+    }
+
+
+    void hasLost()
+    {
+        if(transform.position.y <= 0)
+        {
+            if (isPlayer1)
+            {
+                Debug.Log("Player2 wins");
+            }
+            else
+            {
+                Debug.Log("Player1 wins");
+            }
+        }
+    }
+}
---
+++ AGDD_MultiMicroFury/Assets/Scripts/HasWonCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HasWonCollision : MonoBehaviour {
+
+    private void OnTriggerStart(Collider other)
+    {
+        if (other.GetComponent<BallController>())
+        {
+            if (other.GetComponent<BallController>().isPlayer1)
+            {
+                Debug.Log("Player2 wins");
+            }
+            else
+            {
+                Debug.Log("Player1 wins");
+            }
+        }
+        else
+        {
+            Debug.Log("sdfsdf");
+        }
+    }
+}
