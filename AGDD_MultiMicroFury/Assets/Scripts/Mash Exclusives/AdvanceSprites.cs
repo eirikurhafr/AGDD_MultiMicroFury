f45ee422ab2c40808debcfe84f289e1c76aca692
Ottriman <gogo.gogo520@gmail.com>
2018-02-13 15:13:17 +0000

    Mash Graphics
    Mash Graphics now present, just two squares that go back and forth based
    on mashing.

---
+++ AGDD_MultiMicroFury/Assets/Scripts/Mash Exclusives/AdvanceSprites.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class AdvanceSprites : MonoBehaviour {
+
+    public GameObject sprite1;
+    public GameObject sprite2;
+    public Player1MashButtons player1;
+    public Player2MashButtons player2;
+    public float advance;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Check on if player 1 presses the right button.
+        //Then advance or backtrack progress sprite.
+
+        if (Input.GetKeyDown(player1.mashButton))
+        {
+            sprite1.transform.position += Vector3.right * advance;
+        }
+        else if(Input.anyKeyDown && (!Input.GetKeyDown(player1.mashButton) && !Input.GetKeyDown("j") && !Input.GetKeyDown("k") && !Input.GetKeyDown("l")))
+        {
+            sprite1.transform.position -= Vector3.right * advance;
+        }
+
+        //Do same for player 2
+
+        if (Input.GetKeyDown(player2.mashButton))
+        {
+            sprite2.transform.position += Vector3.right * advance;
+        }
+        else if (Input.anyKeyDown && (!Input.GetKeyDown(player2.mashButton) && !Input.GetKeyDown("a") && !Input.GetKeyDown("s") && !Input.GetKeyDown("d")))
+        {
+            sprite2.transform.position -= Vector3.right * advance;
+        }
+    }
+}
